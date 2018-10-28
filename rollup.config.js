f34079910f0ea2601d53185a75de0a6d951f5727
Jens Lindberg <elusiveunit@gmail.com>
2018-10-28 15:22:31 +0100

    Add minified production builds

--- rollup.config.js
+++ rollup.config.js
@@ -3,27 +3,33 @@ import glob from 'fast-glob';
 
 import babel from 'rollup-plugin-babel';
 import filesize from 'rollup-plugin-filesize';
+import cleanup from 'rollup-plugin-cleanup';
 import resolve from 'rollup-plugin-node-resolve';
 
-export default glob.sync('themes/jp-notes/assets/js/*.js').map((filePath) => ({
-  input: filePath,
-  output: {
-    file: `themes/jp-notes/static/js/${path.basename(filePath)}`,
-    format: 'iife',
-    sourcemap: true,
-    indent: true,
-    // Shortcuts and micro minification boost for commonly used things
-    /* intro: `
-      var win = window;
-      var doc = document;
-      var $ = doc.querySelectorAll;
-    `, */
-  },
-  plugins: [
-    resolve(),
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    filesize(),
-  ],
-}));
+const ENV = process.env.BABEL_ENV;
+
+export default glob.sync('themes/jp-notes/assets/js/*.js').map((filePath) => {
+  const outputFileName = path
+    .basename(filePath)
+    .replace(/\.js$/, ENV === 'production' ? '.min.js' : '.js');
+
+  return {
+    input: filePath,
+    output: {
+      file: `themes/jp-notes/static/js/${outputFileName}`,
+      format: 'iife',
+      sourcemap: true,
+      indent: true,
+    },
+    plugins: [
+      resolve(),
+      babel({
+        exclude: 'node_modules/**',
+      }),
+      filesize(),
+      cleanup({
+        comments: 'none',
+      }),
+    ],
+  };
+});
